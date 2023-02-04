1cc06b3e39aa1867813bca451a75433393e701a4
Salma_El_AIMAR <salmaelaimar02@gmail.com>
2023-02-04 20:09:11 +0000

    added svg icons for signup_screen

--- lib/screens/auth/signup_screen.dart
+++ lib/screens/auth/signup_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
+import 'package:flutter_svg/svg.dart';
 
 class SignupScreen extends StatelessWidget {
   @override
@@ -9,157 +9,175 @@ class SignupScreen extends StatelessWidget {
       body: SafeArea(
         child: Padding(
           padding: EdgeInsets.all(35),
-          child: Column(
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Icon(
-                    Icons.arrow_back_ios_new_rounded,
-                    size: 16,
-                    color: Colors.grey,
-                  ),
-                  Text(
-                    'Sign Up',
-                  ),
-                  Icon(
-                    Icons.arrow_back_ios_new_rounded,
-                    size: 16,
-                    color: Colors.white,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 45,
-              ),
-              Text(
-                'Register Account',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                'Complete your details or continue \nwith social media',
-                style: TextStyle(
-                  color: Colors.grey,
-                  height: 1.5,
-                  fontWeight: FontWeight.w400,
-                ),
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              TextFormField(
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  hintText: "Enter your email",
-                  suffixIcon: Icon(
-                    Icons.email_outlined,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  hintText: "Enter your password",
-                  suffixIcon: Icon(
-                    Icons.lock_outline_rounded,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextFormField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Comfirm password",
-                  hintText: "Re-enter your password",
-                  suffixIcon: Icon(
-                    Icons.lock_outline_rounded,
-                  ),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(50),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              TextButton(
-                onPressed: printSalam,
-                child: Text('Continue'),
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.orange,
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 140,
-                    vertical: 4,
-                  ),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 35,
-                    height: 35,
-                    decoration: BoxDecoration(
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                      size: 16,
                       color: Colors.grey,
                     ),
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Container(
-                    width: 35,
-                    height: 35,
-                    color: Colors.grey,
-                  ),
-                  SizedBox(
-                    width: 10,
-                  ),
-                  Container(
-                    width: 35,
-                    height: 35,
-                    color: Colors.grey,
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                'By continuing your confirm that you agree with our Term and Condition',
-                style: TextStyle(
-                  color: Colors.grey,
-                  height: 1.5,
-                  fontWeight: FontWeight.w400,
+                    Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: "Multi",
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                      size: 16,
+                      color: Colors.white,
+                    ),
+                  ],
                 ),
-                textAlign: TextAlign.center,
-              )
-            ],
+                SizedBox(
+                  height: 45,
+                ),
+                Text(
+                  'Register Account',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  'Complete your details or continue \nwith social media',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    height: 1.5,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 40),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Enter your email",
+                    suffixIcon: Icon(Icons.email_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Enter your password",
+                    suffixIcon: Icon(Icons.lock_outline_rounded),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Confirm Password",
+                    hintText: "Re-enter your password",
+                    suffixIcon: Icon(Icons.lock_outline_rounded),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40),
+                TextButton(
+                  onPressed: printSalam,
+                  child: Text('Continue'),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xfff77546),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 110,
+                      vertical: 20,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 35,
+                      height: 35,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xffeeeff1),
+                      ),
+                      alignment: Alignment.center,
+                      child: SvgPicture.asset(
+                        "assets/icons/google.svg",
+                        width: 25,
+                        height: 25,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 35,
+                      height: 35,
+                      // padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xffeeeff1),
+                      ),
+
+                      alignment: Alignment.center,
+                      child: SvgPicture.asset(
+                        "assets/icons/facebook.svg",
+                        width: 25,
+                        height: 25,
+                      ),
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 35,
+                      height: 35,
+                      padding: EdgeInsets.all(9),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xffeeeff1),
+                      ),
+                      child: SvgPicture.asset(
+                        "assets/icons/twitter.svg",
+                        width: 25,
+                        height: 25,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 14,
+                ),
+                Text(
+                  'By continuing your confirm that you agree with our Term and Condition',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
