8b1c346bef449eac179e9db99374125a927b3dd7
Salma_El_AIMAR <salmaelaimar02@gmail.com>
2023-02-11 18:42:29 +0000

    sign in screen

--- lib/screens/auth/signin_screen.dart
+++ lib/screens/auth/signin_screen.dart
@@ -1,66 +1,223 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 class SigninScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Row(
-            children: [
-              Icon(Icons.arrow_back_ios_new_rounded),
-              Text('Sign In'),
-            ],
-          ),
-          Text('Welcome Back'),
-          Text(
-              'Sign in with your email and password or continue with social media'),
-          TextFormField(),
-          TextFormField(),
-          Row(
-            children: [
-              Row(
-                children: [
-                  Checkbox(
-                    value: true,
-                    onChanged: changeCheckboxState,
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.all(35),
+          child: SingleChildScrollView(
+            physics: BouncingScrollPhysics(),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                      size: 16,
+                      color: Colors.grey,
+                    ),
+                    Text(
+                      'Sign In',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w500,
+                        fontFamily: "Multi",
+                      ),
+                    ),
+                    Icon(
+                      Icons.arrow_back_ios_new_rounded,
+                      size: 16,
+                      color: Colors.white,
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  'Welcome Back',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
                   ),
-                  Text('Remember me'),
-                ],
-              ),
-              Text('Forgot Password'),
-            ],
+                ),
+                SizedBox(
+                  height: 12,
+                ),
+                Text(
+                  'Sign in with your email and password or continue with social media',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    height: 1.5,
+                    fontWeight: FontWeight.w400,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Email",
+                    hintText: "Enter your email",
+                    suffixIcon: Icon(Icons.email_outlined),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: "Password",
+                    hintText: "Enter your password",
+                    suffixIcon: Icon(Icons.lock_outline_rounded),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    Row(
+                      children: [
+                        Checkbox(
+                          value: false,
+                          onChanged: changeCheckboxState,
+                        ),
+                        Text(
+                          'Remember me',
+                          style: TextStyle(
+                            color: Colors.grey,
+                            height: 1.5,
+                            fontWeight: FontWeight.w400,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 45,
+                    ),
+                    Text(
+                      'Forgot Password',
+                      style: TextStyle(
+                        color: Colors.grey,
+                        height: 1.5,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                TextButton(
+                  onPressed: printSalam,
+                  child: Text('Continue'),
+                  style: TextButton.styleFrom(
+                    backgroundColor: Color(0xfff77546),
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 110,
+                      vertical: 20,
+                    ),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 40,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 35,
+                      height: 35,
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xffeeeff1),
+                      ),
+                      alignment: Alignment.center,
+                      /*child: SvgPicture.asset(
+                        "assets/icons/google.svg",
+                        width: 25,
+                        height: 25,
+                      ),*/
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 35,
+                      height: 35,
+                      // padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xffeeeff1),
+                      ),
+
+                      alignment: Alignment.center,
+                      /* child: SvgPicture.asset(
+                        "assets/icons/facebook.svg",
+                        width: 25,
+                        height: 25,
+                      ),
+                      */
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 35,
+                      height: 35,
+                      padding: EdgeInsets.all(9),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Color(0xffeeeff1),
+                      ),
+                      /* child: SvgPicture.asset(
+                        "assets/icons/twitter.svg",
+                        width: 25,
+                        height: 25,
+                      ),
+                      */
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 25,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account? ',
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                    Text(
+                      'Sign Up',
+                      style: TextStyle(
+                        color: Color(0xfff77546),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
-          TextButton(
-            onPressed: printSalam,
-            child: Text('Continue'),
-          ),
-          Row(
-            children: [
-              Container(
-                width: 14,
-                height: 14,
-                color: Colors.blue,
-              ),
-              Container(
-                width: 14,
-                height: 14,
-                color: Colors.red,
-              ),
-              Container(
-                width: 14,
-                height: 14,
-                color: Colors.green,
-              ),
-            ],
-          ),
-          Row(
-            children: [
-              Text('Don\'t have an account? '),
-              Text('Sign Up'),
-            ],
-          ),
-        ],
+        ),
       ),
     );
   }
